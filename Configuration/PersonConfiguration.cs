fe4faf3b27ba109400921b18d2f1f7b900ebb865
Vedad <vedad.keskin98@gmail.com>
2024-03-26 15:58:50 +0100

    Dodavanje konfiguracije

---
+++ Configuration/PersonConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using RentACar.DBContext;
+
+namespace RentACar.Configuration
+{
+    public class PersonConfiguration : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.HasData(
+                new Person
+                {
+                    PersonId = 1,
+                    FirstName = "Vedad",
+                    LastName = "Keskin",
+                    BirthDate = new DateTime(1998, 12, 12),
+                    JMBG = "111111111"
+                },
+                new Person
+                {
+                    PersonId = 2,
+                    FirstName = "Said",
+                    LastName = "Keskin",
+                    BirthDate = new DateTime(2001, 02, 16),
+                    JMBG = "111111111"
+                });
+
+        }
+    }
+}
